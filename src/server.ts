ba16345d12c0ac0114c81e3ff93fa0b0a40e50d9
Arman-Kabir <armankabir8@gmail.com>
2023-11-28 21:59:10 +0600

    typescript added and connection problem solved

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,19 @@
 import mongoose from 'mongoose';
+require('dotenv').config();
 import app from './app';
 
-main().catch(err => console.log(err));
 
 async function main() {
-
     try {
-        // await mongoose.connect(process.env.DATABASE_URL);
-
+        await mongoose.connect(`${process.env.DATABASE_URL}`);
+        console.log('Database connected successfully');
 
         app.listen(process.env.PORT, () => {
             console.log(`Application is listening on port ${process.env.PORT}`)
-          });
+        });
 
     } catch (error) {
-        console.log(`Failed to connect to DB`,error);
+        console.log(`Failed to connect to DB`, error);
     }
 
     // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
